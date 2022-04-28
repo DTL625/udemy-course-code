90c8cfdd5858bc502c0bc00b6196fe59c6c434b5
DTL <dtl@dtl.com>
2022-04-28 11:55:51 +0800

    complete the sample

--- Master the Coding Interview: Data Structures + Algorithms/DSArray/ImplementingArray.py
+++ Master the Coding Interview: Data Structures + Algorithms/DSArray/ImplementingArray.py
@@ -1,15 +1,25 @@
 class MyArray:
+  data = list()
   def __init__(self):
     pass
   def get(self, index):
     pass
   def push(self, item):
+    self.data.append(item)
+    # 等同於 self.data[len(self.data):] = [item]
     pass
-  def pop(self, ):
+  def pop(self):
+    self.data.pop()
+    # 等同於 del(self.data[len(self.data) - 1])
     pass
   def deleteAtIndex(self, index):
+    self.shiftItems(index)
     pass
   def shiftItems(self, index):
+    for idx in range(index, len(self.data) - 1):
+      self.data[idx] = self.data[idx + 1]
+    del(self.data[len(self.data) - 1])
+
     pass
 
 myArray = MyArray()
@@ -21,3 +31,4 @@ myArray.deleteAtIndex(0)
 myArray.push('are')
 myArray.push('nice')
 myArray.shiftItems(0)
+print(myArray.data)
