52a7fafb0a212fe81abc84f349a9e8da39518a56
DTL <dtl@dtl.com>
2023-01-12 22:41:10 +0800

    ch96. 實作-linked list實作 增加prepend

--- Master-the-Coding-Interview-Data-Structures-Algorithms/LinkedList/linkedList.py
+++ Master-the-Coding-Interview-Data-Structures-Algorithms/LinkedList/linkedList.py
@@ -24,7 +24,11 @@ class linkedList:
         self.tail = value
 
     def prepend_node(self, value):
-        pass
+        if not isinstance(value, listNode):
+            value = listNode(value)
+        if self.head is not None:
+            value.next = self.head
+        self.head = value
 
     def print_list(self):
         current = self.head
@@ -38,5 +42,10 @@ ll = linkedList()
 ll.append_node(10)
 ll.append_node(21)
 ll.append_node(12)
+ll.prepend_node(15)
+ll.append_node(4)
+ll.append_node(3)
+ll.prepend_node(31)
+
 ll.print_list()
 
